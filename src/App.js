c3ab222a55f2fdb2300a34b2f58b75add1452707
ArunRathaur1 <arunrathaur92.6@gmail.com>
2024-02-16 11:36:32 +0530

    implemented tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-
+import NevBar from './Components/NevBar';
 function App() {
   return (
-    <div>Radhe Radhe</div>
+    <div>
+    <NevBar></NevBar>
+    </div>
   );
 }
 
---
+++ src/Components/NevBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NevBar() {
+  return (
+    <div>
+    <div className='bg-red-400'>Hello wrld</div>
+    </div>
+  )
+}
