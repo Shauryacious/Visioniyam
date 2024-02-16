d08625b8895a352e65b5e0c03621d68c4cec532d
ArunRathaur1 <arunrathaur92.6@gmail.com>
2024-02-16 13:58:32 +0530

    adjuct home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,12 +6,12 @@ export default function Home(props) {
     <>
     <div style={{ paddingTop: "120px" ,display:"flex" }}>
         <div style={{position:"relative",top:"-20px"}}>
-      <div style={{ position: "relative", left: "10px", fontFamily: "poppins", fontSize: '70px' }}>Control With Eye</div>
-      <div style={{ width: '700px', position: "relative", left: "10px", fontFamily: "poppins", fontWeight: "300",fontSize:"20px" }}>Empower your computer experience with our innovative software, enabling you to effortlessly steer your cursor with nothing but the movement of your eyes.</div>
+      <div style={{ position: "relative", left: "50px", fontFamily: "poppins",fontWeight:"700", fontSize: '50px' }}>Control With Eye</div>
+      <div style={{ width: '500px', position: "relative", left: "50px", fontFamily: "poppins", fontWeight: "400",fontSize:"15px" }}>Empower your computer experience with our innovative software, enabling you to effortlessly steer your cursor with nothing but the movement of your eyes.</div>
       </div>
-      <div><img src={eyeImage} alt="Eye Cursor" style={{position:"relative",top:"-150px",width:"90%"}} /></div>
+      <div><img src={eyeImage} alt="Eye Cursor" style={{position:"relative",top:"-120px",left:"230px",width:"50%"}} /></div>
     </div>
-    <div><button type="button" class="btn btn-primary" style={{color:props.color,background:props.bgColor,position:'relative',top:"-250px",left:"15%"}}>Download software</button></div>
+    <div><button type="button" class="btn btn-primary" style={{color:props.color,background:props.bgColor,position:'relative',top:"-200px",left:"15%"}}>Download software</button></div>
     </>
   );
 }
