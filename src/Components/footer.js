6a83d230cf94f82bb96a5599bc46cb341c90b406
ArunRathaur1 <arunrathaur92.6@gmail.com>
2024-02-16 23:23:20 +0530

    changed

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -5,17 +5,13 @@ export default function footer() {
     <div>
       <div className="card text-center">
   <div className="card-header">
-    Featured
-  </div>
   <div className="card-body">
-    <h5 className="card-title">Special title treatment</h5>
-    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-  <div className="card-footer text-body-secondary">
-    2 days ago
+    <h5 className="card-title">VISIONIYAM</h5>
+    <p className="card-text">A solution for paralyzed and disabled people to access technology.</p>
   </div>
 </div>
+  </div>
+
     </div>
   )
 }
